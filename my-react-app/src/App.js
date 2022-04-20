9058e2dffc8d1bfaa015a64dfe35a6f2b38f053f
unknown <dockadams333@gmail.com>
2022-04-20 14:32:11 +0700

    first

---
+++ my-react-app/src/App.js
@@ -0,0 +1,60 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.title} {props.num}
+    </p>
+  );
+};
+
+const Content = () => {
+  return (
+    <>
+      <Part title="Fundamentals of React" num={10} />
+      <Part title="Using props to pass data" num={7} />
+      <Part title="State of a component" num={14} />
+    </>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      {props.parts[0]["exercises"] +
+        props.parts[1]["exercises"] +
+        props.parts[2]["exercises"]}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
